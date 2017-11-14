89ab00ac4b253ca005ca35d926a53c8f7a6d91fc
bikash-s-bhandari <bikash.bhandari05@gmail.com>
2017-11-14 09:06:08 +0545

    migration created

--- database/migrations/2017_11_14_024307_create_products_table.php
+++ database/migrations/2017_11_14_024307_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->text('details');
+            $table->decimal('price',10,2);
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
